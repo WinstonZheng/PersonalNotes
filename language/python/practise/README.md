cc28e15bcaf5809003b3acfaee0ba8c8ad4df03e
Winston <zhengruin@126.com>
2017-10-05 14:08:34 +0800

    Updates language/python/practise/mkdir-for-md.py
    Auto commit by GitBook Editor

---
+++ language/python/practise/README.md
@@ -0,0 +1,3 @@
+# Use python to slove the problems
+
+- [github markdown 目录产生小工具](/language/python/practise/mkdir-for-md.py)
---
+++ language/python/practise/mkdir-for-md.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+"""
+    it is a simple script to creat a content for github readme.md
+    Usage: python mk-dir-for-md.py RAW_PAGE_FORM_GITHUB
+"""
+
+__author__ = "winston"
+
+import urllib2
+import sys
+import re
+
+
+def getWebFile(url):
+   response = urllib2.urlopen(url)
+   return response
+def valueToUrl(value):
+    ''' change the content vaule to url , so we can link 
+        " " -> "-"
+        "-" or "_" -> same
+        "~`!@#$%^&*()+={}[]|\:;"'<>,.?/" -> ""
+        full-width only "-" is not change, others -> ""
+    '''
+    repalceChar = ['~','`','!','@','#','$','%','^','&','*',
+            '(',')','+','=','{','}','[',']','|','\\',':',';',
+            '\"','\'','<',',','>','.','?','/',
+            '·','！','￥','……','（','）','——','【','】','、','：',
+            '；','“','‘','《','》','，','。','？']
+    value = value.strip()
+    for singleChar in repalceChar:
+        value = value.replace(singleChar, '')
+    # remove the leading and trailing whitespace
+    value = value.replace(' ','-')
+    return value.lower()
+
+def getSameContentNum(content, urlValue):
+    ''' github use the -num to solve the same content problem
+        For example,
+        # haha -> haha
+        # haha -> haha-1
+    '''
+    num = 0
+    if not urlValue:
+        num = 1
+    for line in content:
+        left = line.rfind("#") + 1
+        right = line.rfind(")")
+        key = line[ left : right ]
+        if key == urlValue:
+            num = num + 1
+        if re.match(r'.*-\d$', key):
+            realKey = key[:-2] 
+            if urlValue == realKey: 
+                num = int(key[-1]) + 1
+    return num
+    
+
+
+
+def makeDirFromRawPage( page, isPrint = 1):
+    ''' page is a file-like objext'''
+    InCode = "```"
+    contentTarget  = "#"
+    isInCode = 0 
+    content =[]
+    for line in page.readlines():
+        if line.count(InCode):
+            isInCode = 1
+        if not isInCode and line[0] == contentTarget:
+            count = line.find(" ")
+            value = line[count + 1 : -1]
+            # "# #" is used to back to top
+            # empty string can not work
+            if value == contentTarget or not value:
+                continue
+            # change content value to url
+            urlValue = valueToUrl(value)
+            # same content
+            num = getSameContentNum( content, urlValue)
+            if num:
+                urlValue += ("-" + str(num))
+            contentValue = (count - 1) * "\t" + " [%s](#%s)" % (value , urlValue)
+            if isPrint:
+                print contentValue
+            content.append(contentValue)
+    return content
+            
+
+
+if __name__=='__main__':
+    script, url = sys.argv
+    result = getWebFile(url)
+    content = makeDirFromRawPage(result)
+
+
+
+
+
